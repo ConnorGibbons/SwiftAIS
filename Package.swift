7e649d05e17e82dc73df1f544c8b93b2fcf3b2c7
Connor Gibbons <connorgibbons@comcast.net>
2025-11-04 14:27:07 -0500

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(
         .macOS(.v10_15),
     ],
     dependencies: [
-        .package(url: "https://github.com/ConnorGibbons/RTLSDRWrapper", revision: "9dd86782273d5a8dcd48afb7cc41c85a897b6d7a"),
-        .package(url: "https://github.com/ConnorGibbons/SignalTools", from: "1.0.1"),
-        .package(url: "https://github.com/ConnorGibbons/TCPUtils", from: "1.0.3"),
+        .package(url: "https://github.com/ConnorGibbons/RTLSDRWrapper", revision: "1.0.2"),
+        .package(url: "https://github.com/ConnorGibbons/SignalTools", from: "1.1.1"),
+        .package(url: "https://github.com/ConnorGibbons/TCPUtils", from: "1.0.4"),
     ],
     targets: [
         .executableTarget(
