ad0dc985b1240d2daefd9a24974cc3b9b6760da2
Connor Gibbons <connorgibbons@comcast.net>
2025-11-16 14:25:19 -0500

    Updated SignalTools, removed duplicate functions.

--- Sources/SwiftAIS/Utils/Files.swift
+++ Sources/SwiftAIS/Utils/Files.swift
@@ -24,6 +24,15 @@ struct DemodulationDebugStats: Encodable {
     }
 }
 
+func directoryExists(_ path: String) -> Bool {
+    var isDirObjCBool: ObjCBool = false
+    if FileManager.default.fileExists(atPath: path, isDirectory: &isDirObjCBool) {
+        return isDirObjCBool.boolValue
+    } else {
+        return false
+    }
+}
+
 /// Saves debug stats from AISReceiver as (current time).aisDebug
 /// This is called for each unsuccessful decoding attempt, per-sentence. Can be used to determine what went wrong for a particular demodulation attempt.
 /// Format:
@@ -54,78 +63,6 @@ func writeDebugStats(directoryPath: String, stats: DemodulationDebugStats) {
     }
     catch {
         print("Failed to save debug stats to file, error: \(error.localizedDescription), path: \(newFilePath)")
-    }
-}
-
-func directoryExists(_ path: String) -> Bool {
-    var isDirObjCBool: ObjCBool = false
-    if FileManager.default.fileExists(atPath: path, isDirectory: &isDirObjCBool) {
-        return isDirObjCBool.boolValue
-    } else {
-        return false
-    }
-}
-
-func readIQFromWAV16Bit(fileURL: URL) throws -> [DSPComplex] {
-    let data = try Data(contentsOf: fileURL)
-    
-    var iqOutput: [DSPComplex] = []
-
-    let iqData = data.dropFirst(44)
-    guard iqData.count % 4 == 0 else {
-        print("IQ Data is not properly formatted.")
-        return []
-    }
-    
-    iqData.withUnsafeBytes { (iqDataPtr: UnsafeRawBufferPointer) in
-        let int16ArrayBasePointer = iqDataPtr.bindMemory(to: Int16.self)
-        var currOffset: Int = 0
-        while currOffset < int16ArrayBasePointer.count {
-            let realSample = Float(int16ArrayBasePointer[currOffset]) / 32768.0
-            let imagSample = Float(int16ArrayBasePointer[currOffset + 1]) / 32768.0
-            iqOutput.append(DSPComplex(real: realSample, imag: imagSample))
-            currOffset += 2
-        }
-    }
-    
-    return iqOutput
-}
-
-func readIQFromWAV16Bit(filePath: String) throws -> [DSPComplex] {
-    let fileURL = URL(fileURLWithPath: filePath)
-    let data = try Data(contentsOf: fileURL)
-    var iqOutput: [DSPComplex] = []
-
-    let iqData = data.dropFirst(44)
-    guard iqData.count % 4 == 0 else {
-        print("IQ Data is not properly formatted.")
-        return []
-    }
-    
-    iqData.withUnsafeBytes { (iqDataPtr: UnsafeRawBufferPointer) in
-        let int16ArrayBasePointer = iqDataPtr.bindMemory(to: Int16.self)
-        var currOffset: Int = 0
-        while currOffset < int16ArrayBasePointer.count {
-            let realSample = Float(int16ArrayBasePointer[currOffset]) / 32768.0
-            let imagSample = Float(int16ArrayBasePointer[currOffset + 1]) / 32768.0
-            iqOutput.append(DSPComplex(real: realSample, imag: imagSample))
-            currOffset += 2
-        }
-    }
-    
-    return iqOutput
-}
-
-public func samplesToCSV(_ samples: [DSPComplex], path: String) {
-    var csvText = "I,Q\n"
-    for sample in samples {
-        csvText.append("\(sample.real),\(sample.imag)\n")
-    }
-    do {
-        try csvText.write(toFile: path, atomically: true, encoding: .utf8)
-    }
-    catch {
-        print("Failed to write sample data to csv file.")
     }
 }
 
